3d6cfb800761054e3bdd4bcfb3ea32fce5ea8e93
safarishi <sshsfl@yeah.net>
2016-05-22 09:34:44 +0800

    Update appendCurrentTime.py
    remove sample code for command stub

--- appendCurrentTime.py
+++ appendCurrentTime.py
@@ -1,10 +1,6 @@
 import datetime
 import sublime, sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
-
 class AppendCurrentTimeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command(
